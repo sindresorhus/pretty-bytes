873579db7a0be64cbdc360d972ebb9ad9dece077
meisterlampe <meisterlampe@users.noreply.github.com>
2017-12-04 14:11:06 +0100

    Removed whitespace (again :))...
    ...and use single quoted string. To pass linter checks

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ module.exports = (num, options) => {
 
 	const exponent = Math.min(Math.floor(Math.log10(num) / 3), UNITS.length - 1);
 	let numStr = Number((num / Math.pow(1000, exponent)).toPrecision(3));
-	
+
 	if (options.locale) {
-		if (typeof options.locale === "string") {
+		if (typeof options.locale === 'string') {
 			numStr = numStr.toLocaleString(options.locale);
 		} else if (options.locale) {
 			numStr = numStr.toLocaleString();
