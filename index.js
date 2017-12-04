04ffac1e20be5d32749940d8374637e148d85880
meisterlampe <meisterlampe@users.noreply.github.com>
2017-12-04 13:36:20 +0100

    Ensure options is an object
    Don't check options on each use. Check it at the beginning of the method instead.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ module.exports = (num, options) => {
 	if (!Number.isFinite(num)) {
 		throw new TypeError(`Expected a finite number, got ${typeof num}: ${num}`);
 	}
+	
+	options = Object.assign({}, options);
 
 	const neg = num < 0;
 
@@ -18,7 +20,7 @@ module.exports = (num, options) => {
 
 	const exponent = Math.min(Math.floor(Math.log10(num) / 3), UNITS.length - 1);
 	const numTmp = Number((num / Math.pow(1000, exponent)).toPrecision(3));
-	const numStr = options && options.localize ? numTmp.toLocaleString() : numTmp;
+	const numStr = options.localize ? numTmp.toLocaleString() : numTmp;
 	const unit = UNITS[exponent];
 
 	return (neg ? '-' : '') + numStr + ' ' + unit;
