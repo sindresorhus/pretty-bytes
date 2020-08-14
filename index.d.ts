fdee1c97778ae96e3760fec4d5c2d885dda65eaa
Nathan Moinvaziri <nathan@nathanm.com>
2020-08-13 21:05:59 -0700

    Improved comments for binary option.

--- index.d.ts
+++ index.d.ts
@@ -20,9 +20,9 @@ declare namespace prettyBytes {
 
 		/**
 		Format the number as [bits](https://en.wikipedia.org/wiki/Bit) instead of [bytes](https://en.wikipedia.org/wiki/Byte). This can be useful when, for example, referring to [bit rate](https://en.wikipedia.org/wiki/Bit_rate).
-		
+
 		@default false
-		
+
 		```
 		import prettyBytes = require('pretty-bytes');
 
@@ -31,12 +31,12 @@ declare namespace prettyBytes {
 		```
 		*/
 		readonly bits?: boolean;
-		
+
 		/**
-		Format the number using the [Binary Prefix](https://en.wikipedia.org/wiki/Binary_prefix) instead of the [SI Prefix](https://en.wikipedia.org/wiki/SI_Prefix). This can be useful as memory is much better represented by powers of 2.
-		
+		Format the number using the [Binary Prefix](https://en.wikipedia.org/wiki/Binary_prefix) instead of the [SI Prefix](https://en.wikipedia.org/wiki/SI_Prefix). This can be useful as memory is typically represented by powers of 2. On macOS and Linux file sizes are represented by powers of 10, however on Windows they are represented by powers of 2.
+
 		@default false
-		
+
 		```
 		import prettyBytes = require('pretty-bytes');
 
