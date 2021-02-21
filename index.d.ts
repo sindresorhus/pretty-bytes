5efb81b3ad7834036cce9d350aaf0a481cf05ef0
Sindre Sorhus <sindresorhus@gmail.com>
2021-02-21 20:50:27 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -53,16 +53,19 @@ declare namespace prettyBytes {
 		readonly binary?: boolean;
 
 		/**
-		The minimum number of fraction digits to display. If neither `minimumFractionDigits` or `maximumFractionDigits` are set the default behavior is round to 3 significant digits.
+		The minimum number of fraction digits to display.
+
+		If neither `minimumFractionDigits` or `maximumFractionDigits` are set, the default behavior is to round to 3 significant digits.
 
 		@default undefined
 
 		```
 		import prettyBytes = require('pretty-bytes');
 
-		// Show number with at least 3 fractional digits
+		// Show the number with at least 3 fractional digits
 		prettyBytes(1900, {minimumFractionDigits: 3});
 		//=> '1.900 kB'
+
 		prettyBytes(1900);
 		//=> '1.9 kB'
 		```
@@ -71,16 +74,19 @@ declare namespace prettyBytes {
 
 
 		/**
-		The maximum number of fraction digits to display. If neither `minimumFractionDigits` or `maximumFractionDigits` are set the default behavior is round to 3 significant digits.
+		The maximum number of fraction digits to display.
+
+		If neither `minimumFractionDigits` or `maximumFractionDigits` are set, the default behavior is to round to 3 significant digits.
 
 		@default undefined
 
 		```
 		import prettyBytes = require('pretty-bytes');
 
-		// Show number with at most 1 fractional digit
+		// Show the number with at most 1 fractional digit
 		prettyBytes(1920, {maximumFractionDigits: 1});
 		//=> '1.9 kB'
+
 		prettyBytes(1920);
 		//=> '1.92 kB'
 		```
