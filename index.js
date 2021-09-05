54f3d96bdbc089116de84fa588819686c2cf5da0
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-09-05 16:55:57 +0200

    feat: reduce code (& stack) size
    The execution time of tests done with the previous version and with these changes are around `1,800` (`s.ms`) in both cases.
    
    Signed-off-by: Giuseppe Eletto <peppe.eletto@gmail.com>

--- index.js
+++ index.js
@@ -1,53 +1,5 @@
 'use strict';
 
-const BYTE_UNITS = [
-	'B',
-	'kB',
-	'MB',
-	'GB',
-	'TB',
-	'PB',
-	'EB',
-	'ZB',
-	'YB'
-];
-
-const BIBYTE_UNITS = [
-	'B',
-	'kiB',
-	'MiB',
-	'GiB',
-	'TiB',
-	'PiB',
-	'EiB',
-	'ZiB',
-	'YiB'
-];
-
-const BIT_UNITS = [
-	'b',
-	'kbit',
-	'Mbit',
-	'Gbit',
-	'Tbit',
-	'Pbit',
-	'Ebit',
-	'Zbit',
-	'Ybit'
-];
-
-const BIBIT_UNITS = [
-	'b',
-	'kibit',
-	'Mibit',
-	'Gibit',
-	'Tibit',
-	'Pibit',
-	'Eibit',
-	'Zibit',
-	'Yibit'
-];
-
 /*
 Formats the given number using `Number#toLocaleString`.
 - If locale is a string, the value is expected to be a locale-key (for example: `de`).
@@ -55,64 +7,72 @@ Formats the given number using `Number#toLocaleString`.
 - If no value for locale is specified, the number is returned unmodified.
 */
 const toLocaleString = (number, locale, options) => {
-	let result = number;
-	if (typeof locale === 'string' || Array.isArray(locale)) {
-		result = number.toLocaleString(locale, options);
-	} else if (locale === true || options !== undefined) {
-		result = number.toLocaleString(undefined, options);
-	}
+    if (typeof locale === 'string' || Array.isArray(locale)) {
+        return number.toLocaleString(locale, options);
+    } else if (locale === true || options !== undefined) {
+        return number.toLocaleString(undefined, options);
+    }
 
-	return result;
+    return number;
 };
 
 module.exports = (number, options) => {
-	if (!Number.isFinite(number)) {
-		throw new TypeError(`Expected a finite number, got ${typeof number}: ${number}`);
-	}
+    if (!Number.isFinite(number)) {
+        throw new TypeError(`Expected a finite number, got ${typeof number}: ${number}`);
+    }
 
-	options = Object.assign({bits: false, binary: false}, options);
+    options = Object.assign({bits: false, binary: false}, options);
 
-	const UNITS = options.bits ?
-		(options.binary ? BIBIT_UNITS : BIT_UNITS) :
-		(options.binary ? BIBYTE_UNITS : BYTE_UNITS);
+    const UNITS_FIRSTLETTER = (options.bits ? "b" : "B") + "kMGTPEZY";
 
-	if (options.signed && number === 0) {
-		return ` 0 ${UNITS[0]}`;
-	}
+    if (options.signed && number === 0) {
+        return ` 0 ${UNITS_FIRSTLETTER[0]}`;
+    }
 
-	const isNegative = number < 0;
-	const prefix = isNegative ? '-' : (options.signed ? '+' : '');
+    const isNegative = number < 0;
+    const prefix = isNegative ? '-' : (options.signed ? '+' : '');
 
-	if (isNegative) {
-		number = -number;
-	}
+    if (isNegative) {
+        number = -number;
+    }
 
-	let localeOptions;
+    let localeOptions;
 
-	if (options.minimumFractionDigits !== undefined) {
-		localeOptions = {minimumFractionDigits: options.minimumFractionDigits};
-	}
+    if (options.minimumFractionDigits !== undefined) {
+        localeOptions = {
+            minimumFractionDigits: options.minimumFractionDigits
+        };
+    }
 
-	if (options.maximumFractionDigits !== undefined) {
-		localeOptions = Object.assign({maximumFractionDigits: options.maximumFractionDigits}, localeOptions);
-	}
+    if (options.maximumFractionDigits !== undefined) {
+        localeOptions = Object.assign({
+            maximumFractionDigits: options.maximumFractionDigits
+        }, localeOptions);
+    }
 
-	if (number < 1) {
-		const numberString = toLocaleString(number, options.locale, localeOptions);
-		return prefix + numberString + ' ' + UNITS[0];
-	}
+    if (number < 1) {
+        const numberString = toLocaleString(number, options.locale, localeOptions);
+        return prefix + numberString + ' ' + UNITS_FIRSTLETTER[0];
+    }
 
-	const exponent = Math.min(Math.floor(options.binary ? Math.log(number) / Math.log(1024) : Math.log10(number) / 3), UNITS.length - 1);
-	// eslint-disable-next-line unicorn/prefer-exponentiation-operator
-	number /= Math.pow(options.binary ? 1024 : 1000, exponent);
+    const exponent = Math.min(
+        Math.floor(options.binary ? Math.log(number) / Math.log(1024) : Math.log10(number) / 3),
+        UNITS_FIRSTLETTER.length - 1
+    );
+    // eslint-disable-next-line unicorn/prefer-exponentiation-operator
+    number /= Math.pow(options.binary ? 1024 : 1000, exponent);
 
-	if (!localeOptions) {
-		number = number.toPrecision(3);
-	}
+    if (!localeOptions) {
+        number = number.toPrecision(3);
+    }
 
-	const numberString = toLocaleString(Number(number), options.locale, localeOptions);
+    const numberString = toLocaleString(Number(number), options.locale, localeOptions);
 
-	const unit = UNITS[exponent];
+    let unit = UNITS_FIRSTLETTER[exponent];
+    if (exponent > 0) {
+        unit += options.binary ? "i" : "";
+        unit += options.bits ? "bit" : "B";
+    }
 
-	return prefix + numberString + ' ' + unit;
+    return prefix + numberString + ' ' + unit;
 };
