5a4036dbbd77e6529f9f17ddace3f7b8e1801a5e
Levente Krizsán <lewalevi@gmail.com>
2023-01-24 18:29:38 +0100

    Add missing example tags to existing examples

--- index.d.ts
+++ index.d.ts
@@ -56,6 +56,7 @@ export interface Options {
 
 	@default undefined
 
+	@example
 	```
 	import prettyBytes from 'pretty-bytes';
 
@@ -76,6 +77,7 @@ export interface Options {
 
 	@default undefined
 
+	@example
 	```
 	import prettyBytes from 'pretty-bytes';
 
